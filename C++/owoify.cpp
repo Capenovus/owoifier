24c94ae9dfcb86871ff47727fc74bb03ca6c4898
Boring <57587298+Boring69@users.noreply.github.com>
2021-05-23 17:20:35 +0200

    Update owoify.cpp

--- C++/owoify.cpp
+++ C++/owoify.cpp
@@ -1,22 +1,43 @@
 #include <iostream>
+#include <random>
 
-std::string owoify(std::string STR) {
-	if (STR.empty()) return STR;
-	std::string RSTR = STR;
+//Very bad code
+std::wstring owoify(std::wstring STR) {
+	std::wstring RSTR = STR;
+	std::random_device rd;
+	std::default_random_engine e1(rd());
+	const wchar_t *prefix[] = {
+			L"OWO", L"OwO", L"0w0", L"< 3", L"UwU", L"HIIII!", L"H - hewwo??", L"Huohhhh."
+	};
+	std::uniform_int_distribution<int> pr(0, 8);
+	std::wstring px = prefix[pr(e1)];
 
-	//Very bad code
-	if (RSTR.find("r") != std::string::npos) { size_t pos; while ((pos = RSTR.find("r")) != std::string::npos) { RSTR.replace(pos, 1, "w"); } }
-	if (RSTR.find("l") != std::string::npos) { size_t pos; while ((pos = RSTR.find("l")) != std::string::npos) { RSTR.replace(pos, 1, "w"); } }
-	if (RSTR.find("R") != std::string::npos) { size_t pos; while ((pos = RSTR.find("R")) != std::string::npos) { RSTR.replace(pos, 1, "W"); } }
-	if (RSTR.find("L") != std::string::npos) { size_t pos; while ((pos = RSTR.find("L")) != std::string::npos) { RSTR.replace(pos, 1, "W"); } }
+	const wchar_t *suffix[] = {
+			L"..,", L"fwendo", L"Huoh.", L"._.", L";-;", L";_;", L"(；ω；)", L"ÙωÙ", L"UwU", L"(人◕ω◕)", L"(●´ω｀●)", L"(✿ ♡‿♡)",
+			L"(◠‿◠✿)", L"^ - ^", L"^ _ ^", L"> _ <", L"> _ >", L":P", L":3", L";3", L"x3", L":D", L"xD", L"XDDD", L"（＾ｖ＾）",
+			L"ㅇㅅㅇ", L"(• o •)", L"ʕ•̫͡•ʔ", L"ʕʘ‿ʘʔ", L"(　'◟ ')"
+	};
+	std::uniform_int_distribution<int> sr(0, 30);
+	std::wstring sx = suffix[sr(e1)];
 
-	return RSTR;
+	if (RSTR.find(L"r") != std::string::npos) { size_t pos; while ((pos = RSTR.find(L"r")) != std::string::npos) { RSTR.replace(pos, 1, L"w"); } }
+	if (RSTR.find(L"l") != std::string::npos) { size_t pos; while ((pos = RSTR.find(L"l")) != std::string::npos) { RSTR.replace(pos, 1, L"w"); } }
+	if (RSTR.find(L"R") != std::string::npos) { size_t pos; while ((pos = RSTR.find(L"R")) != std::string::npos) { RSTR.replace(pos, 1, L"W"); } }
+	if (RSTR.find(L"L") != std::string::npos) { size_t pos; while ((pos = RSTR.find(L"L")) != std::string::npos) { RSTR.replace(pos, 1, L"W"); } }
+	if (RSTR.find(L"no") != std::string::npos) { size_t pos; while ((pos = RSTR.find(L"no")) != std::string::npos) { RSTR.replace(pos, 2, L"nu"); } }
+	if (RSTR.find(L"have") != std::string::npos) { size_t pos; while ((pos = RSTR.find(L"have")) != std::string::npos) { RSTR.replace(pos, 4, L"haz"); } }
+	if (RSTR.find(L"has") != std::string::npos) { size_t pos; while ((pos = RSTR.find(L"has")) != std::string::npos) { RSTR.replace(pos, 3, L"haz"); } }
+	if (RSTR.find(L"you") != std::string::npos) { size_t pos; while ((pos = RSTR.find(L"you")) != std::string::npos) { RSTR.replace(pos, 3, L"uu"); } }
+	if (RSTR.find(L"the") != std::string::npos) { size_t pos; while ((pos = RSTR.find(L"the")) != std::string::npos) { RSTR.replace(pos, 3, L"da"); } }
+
+	std::wstring space = L" ";
+	return px + space + RSTR + space + sx;
 }
 
 using namespace std;
 int main()
 {
-	cout << owoify("String to owoify");
+	const wchar_t string[] = L"String to owoify";
+	wcout << owoify(string);
 }
-
 
