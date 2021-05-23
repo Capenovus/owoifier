6327d8b6f05608abf40ea356d8fbee348843155d
Boring <57587298+Boring69@users.noreply.github.com>
2021-05-23 13:50:31 +0200

    C# Integration
    Added C# Library

---
+++ C#/owoifier/owo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace owoifier
+{
+    public class owo
+    {
+        /// <summary>
+        /// owoify a given string
+        /// </summary>
+        /// <param name="String">String to owoify</param>
+        /// <returns>
+        /// owoified String
+        /// </returns>
+        public static string owoify(string String)
+        {
+            if (String == null) throw new ArgumentNullException(nameof(String));
+            var str = String;
+
+            if (!str.ToLower().Contains("l") && !str.ToLower().Contains("r")) return str;
+            if (str.Contains("l")) str = str.Replace("l", "w");
+            if (str.Contains("r")) str = str.Replace("r", "w");
+            if (str.Contains("L")) str = str.Replace("L", "W");
+            if (str.Contains("R")) str = str.Replace("R", "W");
+
+            return str;
+        }
+    } 
+}
