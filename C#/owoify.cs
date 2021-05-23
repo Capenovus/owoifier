d72a2bf2e73cc4be05619b29fc02c984271c5f43
Boring <57587298+Boring69@users.noreply.github.com>
2021-05-23 16:02:28 +0200

    Update owoify.cs

--- C#/owoify.cs
+++ C#/owoify.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace owoifier
+namespace owoify
 {
     public class owo
     {
@@ -14,15 +14,31 @@ namespace owoifier
         public static string owoify(string String)
         {
             if (String == null) throw new ArgumentNullException(nameof(String));
-            var str = String;
 
-            if (!str.ToLower().Contains("l") && !str.ToLower().Contains("r")) return str;
+            var str = String;
+            Random rnd = new Random();
+            string[] prefix =  {
+            "OWO", "OwO", "0w0", "< 3", "UwU", "HIIII!", "H - hewwo??", "Huohhhh."
+            };
+            string[] suffix =
+            {
+                "..,", "fwendo", "Huoh.", "._.", ";-;", ";_;", "(；ω；)", "ÙωÙ", "UwU", "(人◕ω◕)", "(●´ω｀●)", "(✿ ♡‿♡)",
+                "(◠‿◠✿)", "^ - ^", "^ _ ^", "> _ <", "> _ >", ":P", ":3", ";3", "x3", ":D", "xD", "XDDD", "（＾ｖ＾）",
+                "ㅇㅅㅇ", "(• o •)", "ʕ•̫͡•ʔ", "ʕʘ‿ʘʔ", "(　'◟ ')"
+            };
+
             if (str.Contains("l")) str = str.Replace("l", "w");
             if (str.Contains("r")) str = str.Replace("r", "w");
             if (str.Contains("L")) str = str.Replace("L", "W");
             if (str.Contains("R")) str = str.Replace("R", "W");
+            if (str.Contains("no")) str = str.Replace("no", "nu");
+            if (str.Contains("have") || str.Contains("has")) str = str.Replace("have", "haz"); str = str.Replace("has", "haz");
+            if (str.Contains("you")) str = str.Replace("you", "uu");
+            if (str.Contains("the")) str = str.Replace("the", "da");
+
+            str = prefix[rnd.Next(0, prefix.Length)] + " " + str + " " + suffix[rnd.Next(0, suffix.Length)];
 
             return str;
         }
-    } 
+    }
 }
